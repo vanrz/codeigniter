c211b4fd06a8b733ecd2d74569f89ab4d915db1a
Vanessa Ramirez <36080491+vanrz@users.noreply.github.com>
2018-05-25 00:51:49 -0500

    butstrap

--- application/views/formularioEstaciones.php
+++ application/views/formularioEstaciones.php
@@ -2,19 +2,39 @@
 <html lang="en-US">
 <head>
     <meta charset="utf-8">
-    <title>Agregar </title>
-    
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+    <title><?php echo $title; ?></title>
     
 </head>
 <body>
    
-    <form method="post" action="" class="form">
-            <label for="nombre">Title</label>
-            <input type="text" name="estacion" placeholder="Estacion" >   
-            <input type="text" name="descripcion" placeholder="Descripcion" >
-            <input type="text" name="altitud" placeholder="Altitud" >
-            <input type="text" name="latitud" placeholder="Latitud" >
-            <input type="submit" name="postSubmit" value="Aceptar"/>
+	
+	
+	
+    <form method="post" action="" class="form" style="max-width: 500px">
+		
+		<form class="form-inline">
+  			<div class="form-group mb-2">
+    			<label for="nombre" class="sr-only">Agregar:</label>
+    			<input type="text" readonly class="form-control-plaintext" id="" value="Estacion">
+  			</div>
+  			class="sr-only">Estacion </label>
+			<input type="text" class="form-control" id=" " placeholder="Nombre de la estacion ">
+			</div>	
+			<div class="form-group mb-2">
+			<label for="inputdescripcion" class="sr-only">Descripcion:  </label>
+			<input type="text" class="form-control" id=" " placeholder="Describa la estacion">
+			</div>
+			<div class="form-group mb-2">
+			<label for="inputaltitud="sr-only">Altitud: >
+			<input type="text" class="form-control" id=" " placeholder="000.000.000">
+			</div>
+			<div class="form-group mb-2">
+			<label for="inputlatitud" class="sr-only">Latitud:  </label>
+			<input type="text" class="form-control" id=" " placeholder="000.000.000">
+			</div>
+    		<button type="submit" class="btn btn-primary mb-2">AGREGAR</button>
+	  
     </form>
 </body>
 </html>   
